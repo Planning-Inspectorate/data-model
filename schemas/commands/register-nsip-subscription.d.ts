920697999757555a50bf74791190348828760447
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-08 15:22:03 +0000

    chore: remove old .d.ts files

--- schemas/commands/register-nsip-subscription.d.ts
+++
@@ -1,9 +0,0 @@
-import { NSIPSubscription, SubscriptionType } from '../events/nsip-subscription';
-
-/**
- * register-nsip-subscription schema for use in code
- */
-export interface RegisterNSIPSubscription {
-	nsipSubscription: NSIPSubscription;
-	subscriptionTypes: SubscriptionType[];
-}

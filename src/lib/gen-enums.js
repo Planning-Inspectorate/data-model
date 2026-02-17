ec170c58246d6262361c0ad735cc8cd9d8b3f304
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-02-17 17:07:11 +0000

    chore: move gen-enums logic to gen-enum-impl.js

--- src/lib/gen-enums.js
+++ src/lib/gen-enums.js
@@ -1,16 +1,14 @@
 import fs from 'fs/promises';
 import path from 'path';
-import url from 'url';
 import { loadAllSchemas } from '../index.js';
-import snakeCase from 'lodash.snakecase';
 import { loadSchema } from './load.js';
+import { collectEnumProps, generateConstants, schemaPrefix } from './gen-enum-impl.js';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const __dirname = import.meta.dirname;
 
 export const constPath = path.join(__dirname, '..', 'enums.js');
 export const constTsPath = path.join(__dirname, '..', 'enums.d.ts');
 
-const SPACING = '  ';
 const NEW_LINE = '\n';
 
 /**
@@ -42,82 +40,7 @@ async function run() {
 	await fs.writeFile(constTsPath, tsOutput);
 }
 
-/**
- * Generate JS code to define exported constants for the given enum values
- *
- * @param {string} name
- * @param {string[]} values
- * @param {Object} opts
- * @param {boolean} [opts.ts] - whether to generate the TS type or JS code
- * @returns {string}
- */
-function generateConstants(name, values, { ts = false } = {}) {
-	const freezeStart = ts ? '' : 'Object.freeze(';
-	const freezeEnd = ts ? ' as const' : ')';
-	const exportPrefix = 'export ';
-	const lines = [`${exportPrefix}const ${formatName(name)} = ${freezeStart}{`];
-	/**
-	 * @param {string} line
-	 * @param {number} spacing
-	 */
-	const addWithSpace = (line, spacing = 0) => lines.push(SPACING.repeat(spacing) + line);
-
-	for (const value of values.sort()) {
-		addWithSpace(`${formatName(value)}: '${value}',`, 1);
-	}
-
-	lines.push(`}${freezeEnd};`);
-
-	return lines.join(NEW_LINE);
-}
-
-/**
- * Format a name for use as a constant value: UPPER_SNAKE_CASE
- *
- * @param {string} name
- * @returns {string}
- */
-function formatName(name) {
-	return snakeCase(name).toUpperCase();
-}
-
-/**
- *
- * @param {Object<string, string[]>} map
- * @param {Object<string, {type?: string|string[], enum?: string[]}>} properties
- * @param {string} [prefix]
- */
-function collectEnumProps(map, properties = {}, prefix) {
-	for (const [key, prop] of Object.entries(properties)) {
-		const name = prefix ? prefix + '_' + key : key;
-
-		const hasStringEnum = (p) =>
-			p && p.enum && Array.isArray(p.enum) && p.type && (p.type === 'string' || p.type.includes('string'));
-		const getEnumValues = (p) => p.filter((e) => e !== null);
-		const collectEnumFromProp = (p) => {
-			if (hasStringEnum(p)) {
-				map[name] = getEnumValues(p.enum);
-			}
-		};
-
-		collectEnumFromProp(prop);
-		collectEnumFromProp(prop.items);
-	}
-}
-
-/**
- * Generate a prefix based on the schema name
- *
- * @param {string} schemaName
- * @returns {string|undefined}
- */
-function schemaPrefix(schemaName) {
-	if (schemaName.toLowerCase().includes('appeal')) {
-		return 'appeal';
-	}
-	if (schemaName.toLowerCase().includes('nsip')) {
-		return 'nsip';
-	}
-}
-
-run().catch(console.error);
+run().catch((err) => {
+	console.error(err);
+	process.exit(1);
+});

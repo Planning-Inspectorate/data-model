79b4dd199f8a0637b7d151fa9a5034763c0672d4
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-25 13:49:56 +0100

    chore: use @planning-inspectorate/coding-standards

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,4 @@
-import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from 'globals';
+import { eslintConfig } from '@planning-inspectorate/coding-standards';
+import { defineConfig } from 'eslint/config';
 
-export default [
-	{
-		...js.configs.recommended,
-		ignores: ['**/*.cjs', 'node_modules/**']
-	},
-	{
-		ignores: ['node_modules/**', '**/*.cjs'],
-		languageOptions: {
-			ecmaVersion: 2025,
-			sourceType: 'module',
-			globals: {
-				...globals.node
-			}
-		}
-	},
-	eslintConfigPrettier
-];
+export default defineConfig(eslintConfig);
